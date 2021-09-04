48385edc165e34254c23dc2879d2d15e71e5b2c1
Vaibhav Sompura <42895093+vsompura3@users.noreply.github.com>
2021-09-04 23:54:05 +0530

    Added navigation

---
+++ app/script.js
@@ -0,0 +1,20 @@
+const openMenuButton = document.querySelector('#menu-open');
+const closeMenuButton = document.querySelector('#menu-close');
+const menu = document.querySelector('.mobile-menu');
+const body = document.querySelector('body');
+
+function openMenu() {
+  menu.classList.add('open');
+  // Stop scrolling page while menu is open
+  body.style.overflow = 'hidden';
+}
+
+function closeMenu() {
+  menu.classList.remove('open');
+  body.style.overflow = 'auto';
+
+}
+
+openMenuButton.addEventListener('click', openMenu)
+closeMenuButton.addEventListener('click', closeMenu)
+
